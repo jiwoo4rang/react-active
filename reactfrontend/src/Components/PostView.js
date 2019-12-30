2037cb69f620beedac6af8b217bc8f61bd9f3fab
jiwoo4rang <48649107+jiwoo4rang@users.noreply.github.com>
2019-12-30 17:11:03 +0900

    firstcommit

---
+++ reactfrontend/src/Components/PostView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const dummy_prop = {
+    title: '테스트용 타이틀',
+    content: '테스트용 글',
+}
+
+class PostView extends Component {
+    render() {
+        const {id, title, content} = this.props 
+        return (
+            <div>
+                {id}
+                <h3>{title}</h3>
+                <p>{content}</p>
+            </div>
+        );
+    }
+}
+export default PostView;
